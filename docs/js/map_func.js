261c877d42f19a4b7b6f2bc3c6a80273d32c9c90
Eric Chang <echang15@gmail.com>
2017-02-19 23:10:07 -0500

    map

--- docs/js/map_func.js
+++ docs/js/map_func.js
@@ -2,15 +2,17 @@ jQuery(function($) {
 	"use strict";
     // Asynchronously Load the map API 
     var script = document.createElement('script');
-    script.src = "https:///maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
+    script.src = "https:///maps.googleapis.com/maps/api/js?key=AIzaSyCA86jBOywV0Gr5dtvyD1aYl4N6Ju4m9wE&callback=initialize";
     document.body.appendChild(script);
 });
 
 // This example creates a 2-pixel-wide red polyline showing
 // the path of William Kingsford Smith's first trans-Pacific flight between
 // Oakland, CA, and Brisbane, Australia.
+//API KEY AIzaSyCA86jBOywV0Gr5dtvyD1aYl4N6Ju4m9wE
 
 function initialize() {
+  var pickup = { lat:32.3055977, lng: -64.8669904}
   var mapOptions = {
     zoom: 16,
 	scrollwheel: false,
@@ -21,7 +23,12 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById('map_canvas'),
       mapOptions);
 
+  var marker = new google.maps.Marker({
+    position: pickup,
+    map: map
+  });
+
 
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+//google.maps.event.addDomListener(window, 'load', initialize);
